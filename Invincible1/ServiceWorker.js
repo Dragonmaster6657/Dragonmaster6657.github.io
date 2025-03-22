440f7097b2c7bf590f28b535e452f66acc87d003
Dragonmaster6657 <spinosaurusdylan@gmail.com>
2025-03-22 14:02:44 -0600

    Add files via upload

---
+++ Invincible1/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-InvincibleGame-0.1.0";
+const contentToCache = [
+    "Build/Invincible1.loader.js",
+    "Build/Invincible1.framework.js.unityweb",
+    "Build/Invincible1.data.unityweb",
+    "Build/Invincible1.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
